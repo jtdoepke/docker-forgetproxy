82b7cc9b943c51f4a5fa9eaa5b2fa6fb955113ed
Jaye Doepke <jdoepke@mintel.com>
2017-02-23 19:54:25 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run -ti --net=host --privileged -e http_proxy=$http_proxy -e https_proxy=$https_proxy klabs/forgetproxy
+docker run -ti --net=host --privileged -e http_proxy=$http_proxy -e https_proxy=$https_proxy jtdoepke/forgetproxy
